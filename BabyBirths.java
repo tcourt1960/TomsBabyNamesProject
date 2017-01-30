ccac2eb772f6069961d867285a4cb6588de9b2ab
Tom Courtney <tpcourtney@gmail.com>
2017-01-30 16:46:53 -0500

    first commit

---
+++ BabyBirths.java
@@ -0,0 +1,129 @@
+/**
+ * Print out total number of babies born, as well as for each gender, in a given CSV file of baby name data.
+ * 
+ * @author Duke Software Team 
+ */
+import edu.duke.*;
+import org.apache.commons.csv.*;
+import java.io.*;
+
+public class BabyBirths {
+    public void printNames () {
+        FileResource fr = new FileResource("data/example-small.csv");
+        for (CSVRecord rec : fr.getCSVParser(false)) {
+            int numBorn = Integer.parseInt(rec.get(2));
+            if (numBorn <= 100) {
+                System.out.println("Name " + rec.get(0) +
+                           " Gender " + rec.get(1) +
+                           " Num Born " + rec.get(2));
+            }
+        }
+    }
+
+    public void totalBirths (FileResource fr) {
+        int totalBirths = 0;
+        int totalBoys = 0;
+        int totalGirls = 0;
+        for (CSVRecord rec : fr.getCSVParser(false)) {
+            int numBorn = Integer.parseInt(rec.get(2));
+            totalBirths += numBorn;
+            if (rec.get(1).equals("M")) {
+                totalBoys += numBorn;
+            }
+            else {
+                totalGirls += numBorn;
+            }
+        }
+        System.out.println("total births = " + totalBirths);
+        System.out.println("female girls = " + totalGirls);
+        System.out.println("male boys = " + totalBoys);
+    }
+
+    public void testTotalBirths () {
+        //FileResource fr = new FileResource();
+        FileResource fr = new FileResource("data/yob2014.csv");
+        totalBirths(fr);
+    }
+    
+                
+    
+    public int getRank(DirectoryResource dr, int year, String name, String gender){
+        int currentRank = 1;
+        int theRank = -1;
+       
+        
+        for (File f : dr.selectedFiles()) {
+		    FileResource fr = new FileResource(f);
+            String currentYearString = f.getName();
+            
+            if(currentYearString.contains(Integer.toString(year))){
+            
+                for (CSVRecord rec : fr.getCSVParser(false)){
+                    if(rec.get(1).equals(gender) && rec.get(0).equals(name)){
+                        theRank = currentRank;
+                        System.out.println("currentYearString" + " " + theRank);
+                        return theRank;
+                    }
+                    currentRank += 1;
+                }
+            }
+        }
+        
+        return theRank;
+    }
+    
+    public void testgetRank(){
+          int theRank = -1;
+          int year = 2010;
+          String name = "Sophia";
+          String gender = "F";
+          
+          DirectoryResource dr = new DirectoryResource();
+          theRank=getRank(dr, year, name,gender);
+          System.out.println(name + " rank is " + theRank + " in " + year);
+    }
+
+    public String getName(int year, int rank, String gender){
+     String theName = "NO NAME";
+     
+        DirectoryResource dr = new DirectoryResource();
+        
+        for (File f : dr.selectedFiles()) {
+		    FileResource fr = new FileResource(f);
+            String currentYearString = f.getName();
+            
+            if(currentYearString.contains(Integer.toString(year))){
+            
+                for (CSVRecord rec : fr.getCSVParser(false)){
+                    if(rec.get(1).equals(gender) && (getRank(dr, year,rec.get(0),gender)==rank)){
+                        theName = rec.get(0);
+                        System.out.println(theName);
+                        return theName;
+                    }
+                }
+            }
+        }
+        return theName;
+    }
+    
+    public void testgetName(){
+          int year = 2010;
+          int rank = 3;
+          String gender = "F";
+          String theName=getName(year, rank, gender);
+          
+          System.out.println(" Name is " + theName + " in " + year);
+    }
+    
+    
+    public void checkDir(){
+    DirectoryResource dr = new DirectoryResource();
+    
+        for (File f : dr.selectedFiles()) {
+            FileResource fr = new FileResource(f);
+            System.out.println(f.getName());   
+        }
+    }
+    
+}
+
